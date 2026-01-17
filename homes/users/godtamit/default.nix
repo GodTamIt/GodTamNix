1a4b82549d651f26e7d6079878b983b352ce6f36
Christopher Tam <ohgodtamit@gmail.com>
2026-01-17 12:20:56 -0500

    IceCube: Stop auto-starting hyprland from fish

--- homes/users/godtamit/default.nix
+++ homes/users/godtamit/default.nix
@@ -45,10 +45,6 @@
         set -x NIX_PATH nixpkgs=channel:nixos-unstable
         set -x NIX_LOG info
         set -x TERMINAL kitty
-
-        if test (tty) = "/dev/tty1"
-          exec Hyprland &> /dev/null
-        end
       '';
 
       shellAbbrs = {
