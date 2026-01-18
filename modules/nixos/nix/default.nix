a81fc1688d0ce9f63deea8e2d6549a4e93ba0bd0
Christopher Tam <ohgodtamit@gmail.com>
2026-01-18 09:09:51 -0500

    nix: Add CUDA binary cache to configuration

--- modules/nixos/nix/default.nix
+++ modules/nixos/nix/default.nix
@@ -59,6 +59,7 @@ in {
           "https://nix-gaming.cachix.org"
           "https://nixpkgs-wayland.cachix.org"
           "https://attic.xuyh0120.win/lantian"
+          "https://cache.nixos-cuda.org"
           "https://cache.nixos.org"
         ];
         trusted-public-keys = [
@@ -67,6 +68,7 @@ in {
           "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
           "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
           "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
+          "https://cache.nixos-cuda.org"
           "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         ];
       };
