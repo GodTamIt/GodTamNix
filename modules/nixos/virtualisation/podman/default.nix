3abebcd4a0cb40b75795d1ee5019928565e1fe84
Christopher Tam <ohgodtamit@gmail.com>
2026-02-09 16:39:15 -0500

    virtualisation: Add support for qemu `binfmt` emulation

--- modules/nixos/virtualisation/podman/default.nix
+++ modules/nixos/virtualisation/podman/default.nix
@@ -4,15 +4,26 @@
   pkgs,
   ...
 }: let
-  inherit (lib) mkIf;
+  inherit (lib) mkIf mkOption types;
 
   cfg = config.godtamnix.virtualisation.podman;
 in {
   options.godtamnix.virtualisation.podman = {
     enable = lib.mkEnableOption "podman";
+
+    emulatedSystems = mkOption {
+      type = types.listOf types.str;
+      default = ["aarch64-linux"];
+      description = "List of systems to emulate via binfmt (e.g. for cross-architecture container builds).";
+    };
   };
 
   config = mkIf cfg.enable {
+    boot.binfmt = {
+      inherit (cfg) emulatedSystems;
+      preferStaticEmulators = true;
+    };
+
     environment.systemPackages = with pkgs; [
       podman-compose
     ];
