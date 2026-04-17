9803a5138996ec686cd63cef9d6647f8c499c7ab
Christopher Tam <ohgodtamit@gmail.com>
2026-04-17 10:29:36 -0400

    Shaq: Add IceCube keys to authorized keys

--- systems/x86_64-linux/Shaq/users.nix
+++ systems/x86_64-linux/Shaq/users.nix
@@ -20,6 +20,7 @@
 
         authorizedKeys = [
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHQWgY055bGa/WPslldQ6Li/VGg8Zfvk1qqrJelLcJWZ godtamit@Chriss-MBP.local"
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINVma8utCSpsAs4XDWYESKGSs+Wc7PKtUspMUPaD36kn godtamit@IceCube"
         ];
       };
 
